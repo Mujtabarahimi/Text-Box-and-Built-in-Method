40b6aae6f5c6fa4a448b1054d63274dbbfec7d9f
Mujtabarahimi <134372842+Mujtabarahimi@users.noreply.github.com>
2023-05-25 14:22:08 +0430

    Add files via upload

---
+++ gethub/Built in Method/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // built in     method
+            double  a   =Math.Sqrt(4);
+            Console.WriteLine("square root of 4:"+a);
+            System.Console.ReadKey();
+        }
+    }
+}
